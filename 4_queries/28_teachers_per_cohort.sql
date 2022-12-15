b99e8b1f62626e7091f45f309e3b97557c707b34
Jade <jade.duong@telus.com>
2022-12-14 21:34:48 -0800

    add 28 & 29 in 4_queries

--- 4_queries/28_teachers_per_cohort.sql
+++ 4_queries/28_teachers_per_cohort.sql
@@ -1,9 +1,17 @@
-/*SELECT teachers.name as teacher
+/*SELECT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE assistance_requests.teacher_id 
-GROUP BY teachers.name
+WHERE cohorts.name ='JUL02'
+GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;*/
 
+
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teacher;
