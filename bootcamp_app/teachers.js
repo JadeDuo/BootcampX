a482870bca28c05f250092c89c66d53597e6eba1
Jade <jade.duong@telus.com>
2022-12-15 13:03:47 -0800

    update students and teacher files to sanitize inputs

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,6 +6,8 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 const args = process.argv.slice(2)
+const cohortName = args[0]
+const values = [`${cohortName}`]
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -13,9 +15,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
